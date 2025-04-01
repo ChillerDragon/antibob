2e712049d8b43e16814174bf55bc71527d8b3f94
ChillerDragon <ChillerDragon@gmail.com>
2025-04-01 19:32:50 +0200

    Is overlapping with teehistorian fine? who knows

--- src/bob/protocol_ex.h
+++ src/bob/protocol_ex.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <engine/shared/protocol_ex.h>
+
 enum
 {
-	__MAGIC_OFFSET_TO_NOT_CONFLICT_WITH_DDNET = 90000,
+	__MAGIC_OFFSET_TO_NOT_CONFLICT_WITH_DDNET = OFFSET_TEEHISTORIAN_UUID - 1,
 #define UUID(id, name) id,
 #include <bob/protocol_ex_msgs.h>
 #undef UUID
