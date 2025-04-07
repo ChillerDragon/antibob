1fbe5d33315ff1a6e82d42c9f955b20173f90abf
ChillerDragon <chillerdragon@gmail.com>
2025-04-07 17:42:50 +0200

    Use bob uuid manager

--- src/bob/protocol_ex.h
+++ src/bob/protocol_ex.h
@@ -2,10 +2,23 @@
 
 #include <game/mapitems_ex.h>
 
+#include <bob/uuid_manager.h>
+
+class CMsgPacker;
+class CUnpacker;
+
 enum
 {
-	__MAGIC_OFFSET_TO_NOT_CONFLICT_WITH_DDNET = END_MAPITEMTYPES_UUID - 1,
+	__NETMSG_BOB_UUID_HELPER = OFFSET_NETMSG_UUID - 1,
 #define UUID(id, name) id,
 #include <bob/protocol_ex_msgs.h>
 #undef UUID
 };
+
+// reimplementation of ddnet's RegisterUuids
+// to be independent in the antibot module
+void BobRegisterUuids(CBobUuidManager *pManager);
+
+// reimplementation of ddnet's UnpackMessageId
+// to be independent in the antibot module
+int BobUnpackMessageId(int *pId, bool *pSys, CUuid *pUuid, CUnpacker *pUnpacker, CMsgPacker *pPacker);
