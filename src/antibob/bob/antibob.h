616d9cfb0c23f61e889374b8b8443a7629f49ea7
ChillerDragon <chillerdragon@gmail.com>
2025-04-28 17:09:33 +0200

    Allow special hook overwrite

--- src/antibob/bob/antibob.h
+++ src/antibob/bob/antibob.h
@@ -35,10 +35,10 @@ public:
 	//
 
 	// return true to drop the message
-	bool OnSayNetMessage(const CNetMsg_Cl_Say *pMsg, int ClientId, const CUnpacker *pUnpacker);
+	virtual bool OnSayNetMessage(const CNetMsg_Cl_Say *pMsg, int ClientId, const CUnpacker *pUnpacker);
 
 	// return true to drop the message
-	bool OnSayNetMessage7(const protocol7::CNetMsg_Cl_Say *pMsg, int ClientId, const CUnpacker *pUnpacker);
+	virtual bool OnSayNetMessage7(const protocol7::CNetMsg_Cl_Say *pMsg, int ClientId, const CUnpacker *pUnpacker);
 
 	//
 	// ddnet antibot interface hooks
