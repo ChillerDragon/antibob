1a7ef51695972d2ae514b735bd669d697782d7a2
ChillerDragon <chillerdragon@gmail.com>
2026-07-28 13:18:30 +0200

    Add failing test for arg parser
    https://github.com/ChillerDragon/antibob/issues/47

--- src/test/bob/console.cpp
+++ src/test/bob/console.cpp
@@ -11,6 +11,32 @@ void ExpectEscape(CBobTest &BobTest, const char *pInput, const char *pEscaped)
 	EXPECT_STREQ(aEscaped, pEscaped);
 }
 
+void TestArgParse(CBobTest &BobTest)
+{
+	bool Ok;
+	char aError[512];
+
+	{
+		CBobResult Result("hello world", "ss");
+		Ok = Result.ParseArgs(aError, sizeof(aError));
+		EXPECT_EQ(Ok, true);
+	}
+
+	{
+		CBobResult Result("hello world too many args", "ss");
+		Ok = Result.ParseArgs(aError, sizeof(aError));
+		EXPECT_EQ(Ok, false);
+		EXPECT_STREQ(aError, "got 5 out of 2 arguments ss");
+	}
+
+	{
+		CBobResult Result("foo \"hello world\" bar", "sss");
+		Ok = Result.ParseArgs(aError, sizeof(aError));
+		EXPECT_EQ(Ok, true);
+		EXPECT_STREQ(aError, "");
+	}
+}
+
 int main()
 {
 	CBobTest BobTest;
@@ -26,4 +52,6 @@ int main()
 	ExpectEscape(BobTest, "foo\\\"bar", "foo\\\\\\\"bar");
 	ExpectEscape(BobTest, "'\"'", "'\\\"'");
 	ExpectEscape(BobTest, "\";shutdown", "\\\";shutdown");
+
+	TestArgParse(BobTest);
 }
